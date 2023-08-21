478dd59bd3d5146266c2df063a5d0d72b1925011
ahmedragab <ahmedragab20901@gmail.com>
2023-08-21 14:24:09 +0300

    v

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-import "./js/generators";
+import { ref, computed } from "@vue/reactivity";
+
+const btnEl = document.querySelector("button");
+
+const count = ref(0);
+
+console.log(count.value);
+
+const double = computed(() => count.value * 2);
+
+const increment = () => {
+  count.value++;
+
+  console.log(count.value);
+  console.log(double.value);
+};
+
+btnEl.addEventListener("click", increment);
